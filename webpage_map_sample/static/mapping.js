e8bd5e8151fdbf5e2a70c14667092fb92dc5b443
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-04-17 23:35:50 +1200

    Add comments for clarity

--- webpage_map_sample/static/mapping.js
+++ webpage_map_sample/static/mapping.js
@@ -1,16 +1,21 @@
+// Initialise Map
 var mymap = L.map('main-map', {
     zoomControl: false,
     center: [lat, lon],
     zoom: 15
 })
 
+// Disable zooming and dragging
 // mymap.dragging.disable()
 // mymap.touchZoom.disable()
 // mymap.doubleClickZoom.disable()
 // mymap.scrollWheelZoom.disable()
 // mymap.boxZoom.disable()
+
+// Save coordinates in an array
 var savedCoordinates = []
 
+// Add standard layer
 var osm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -20,6 +25,7 @@ var osm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?a
     zoomOffset: -1,
 }).addTo(mymap);
 
+// Add satellite layer
 var sat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     maxZoom: 18,
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
@@ -27,11 +33,13 @@ var sat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Worl
     zoomOffset: -1,
 }).addTo(mymap);
 
+// Add layers to base layer
 var baseLayers = {
     'Standard': osm,
     'Satellite': sat
 }
 
+// Add controls to switch between layers
 L.control.layers(
     baseLayers,
     {},
@@ -40,10 +48,12 @@ L.control.layers(
     }
 ).addTo(mymap)
 
+// Add zoom controls
 L.control.zoom({
     position: 'topright'
 }).addTo(mymap)
 
+// Add markers on the map
 for (var i = 0; i < coordinates.length; i++) {
     savedCoordinates.push(L.marker([coordinates[i].lat, coordinates[i].lng])
         .addTo(mymap)
@@ -51,6 +61,7 @@ for (var i = 0; i < coordinates.length; i++) {
     )
 }
 
+// Dynamically change elements
 mymap.on('mousemove', function(event) {
     document.getElementById('side-pane-content').innerHTML = event.latlng.lat + "<br>" + event.latlng.lng
 })
